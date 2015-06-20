dfc5d3afe8e6c259b14120152b3ef549a3e9cc32
Matt Bruzek <matthew.bruzek@canonical.com>
2015-06-20 17:46:12 -0500

    Changes with the test-service output.

--- runner.go
+++ runner.go
@@ -3,35 +3,59 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 )
 
+const milliBetweenRequests = 10
+
 func main() {
+
 	if len(os.Args) > 1 {
-		url := os.Args[1]
+		url, err := url.Parse(os.Args[1])
+		if err != nil {
+			fmt.Printf("Invalid url %s\n", os.Args[1])
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		generateTraffic(url)
 	}
 }
 
-func generateTraffic(url string) {
+func generateTraffic(target *url.URL) {
 	for {
-		time.Sleep(2 * time.Second)
-		Get(url)
+		randomInt := RandInt(1, 1000)
+		time.Sleep(milliBetweenRequests * time.Millisecond)
+		// Append a random number on on the path section of the url.
+		target.Path = fmt.Sprintf("%v", randomInt)
+		Get(target)
 	}
 }
 
-func Get(url string) {
-	response, err := http.Get(url)
+func Get(target *url.URL) {
+	log.Println(target)
+	// Make the GET request to the target.
+	response, err := http.Get(target.String())
 	if err != nil {
 		fmt.Println(err)
 	}
+	// Close the body once this variable gets out of scope.
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println(err)
+	if !strings.Contains(string(body), target.Path) {
+		log.Println("Response body did not contain " + target.Path)
 	}
-	fmt.Println(url)
-	fmt.Printf("%s", body)
+	log.Printf("%s %d\n", response.Status, response.StatusCode)
+}
+
+func RandInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
 }
